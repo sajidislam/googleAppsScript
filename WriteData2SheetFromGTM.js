2e18f83c03acd5856dbc964d2a67c149bead438f
Sajid Islam <sajidislam@gmail.com>
2023-11-13 12:34:53 -0500

    Update WriteData2SheetFromGTM.js
    1. Better formatted code
    2. Added logging of exception

--- WriteData2SheetFromGTM.js
+++ WriteData2SheetFromGTM.js
@@ -6,6 +6,7 @@ var SHEET_KEY = "<ENTERT_YOUR_SHEET_KEY>";
 var SECRET_KEY = "<SECRET_KEY>";
 var EXPECTED_REFERER = "<PUT_YOUR_DOMAIN_NAME_FOR_ADDED_SECURITY>";
 
+
 // 2. Run &gt; setup
 //
 // 3. Publish &gt; Deploy as web app
@@ -24,14 +25,14 @@ return handleResponse(e);
 }
 
 function doPost(e){
-return handleResponse(e);
-}
+  return handleResponse(e);
+  }
 
 function handleResponse(e) {
-var lock = LockService.getPublicLock();
-lock.waitLock(30000); // wait 30 seconds before conceding defeat.
+  var lock = LockService.getPublicLock();
+  lock.waitLock(30000); // wait 30 seconds before conceding defeat.
 
-  Logger.log("Received secret_key: " + e.parameter.secret_key);
+  //Logger.log("Received secret_key: " + e.parameter.secret_key);
   //Logger.log("Received custom_referer: " + e.parameter.custom_referer);
 
   // Verify Source by checking secret key
@@ -40,7 +41,7 @@ lock.waitLock(30000); // wait 30 seconds before conceding defeat.
     return ContentService
       .createTextOutput(JSON.stringify({"result":"error", "error": "Unauthorized - Invalid Secret Key"}))
       .setMimeType(ContentService.MimeType.JSON);
-  }
+    }
 
 // Check custom_referer
 var customReferer = e.parameter.custom_referer || "";
@@ -50,37 +51,38 @@ if (customReferer !== EXPECTED_REFERER) {
     .setMimeType(ContentService.MimeType.JSON);
 }
 
-
 try {
-// next set where we write the data - you could write to multiple/alternate destinations
-var doc = SpreadsheetApp.openById(SHEET_KEY);
-var sheet = doc.getSheetByName(SHEET_NAME);
+   // next set where we write the data - you could write to multiple/alternate destinations
+   var doc = SpreadsheetApp.openById(SHEET_KEY);
+   var sheet = doc.getSheetByName(SHEET_NAME);
 
-// we'll assume header is in row 1 but you can override with header_row in GET/POST data
-var headRow = e.parameter.header_row || 1;
-var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
-var nextRow = sheet.getLastRow()+1; // get next row
-var row = [];
-// loop through the header columns
-for (i in headers){
-if (headers[i] == "Timestamp"){ // special case if you include a 'Timestamp' column
-row.push(new Date());
-} else { // else use header name to get data
-row.push(e.parameter[headers[i]]);
-}
-}
-// more efficient to set values as [][] array than individually
-sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);
-// return json success results
-return ContentService
-.createTextOutput(JSON.stringify({"result":"success", "row": nextRow}))
-.setMimeType(ContentService.MimeType.JSON);
-} catch(e){
-// if error return this
-return ContentService
-.createTextOutput(JSON.stringify({"result":"error", "error": e}))
-.setMimeType(ContentService.MimeType.JSON);
-} finally { //release lock
-lock.releaseLock();
-}
-}
+   // we'll assume header is in row 1 but you can override with header_row in GET/POST data
+   var headRow = e.parameter.header_row || 1;
+   var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+   var nextRow = sheet.getLastRow()+1; // get next row
+   var row = [];
+   // loop through the header columns
+   for (i in headers){
+      if (headers[i] == "Timestamp"){ // special case if you include a 'Timestamp' column
+        row.push(new Date());
+      } else { // else use header name to get data
+        row.push(e.parameter[headers[i]]);
+    }
+  }
+   // more efficient to set values as [][] array than individually
+   sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);
+   // return json success results
+   return ContentService
+    .createTextOutput(JSON.stringify({"result":"success", "row": nextRow}))
+    .setMimeType(ContentService.MimeType.JSON);
+  } catch(e){
+    // if error return this
+    // Log the error
+    Logger.log(e);
+    return ContentService
+      .createTextOutput(JSON.stringify({"result":"error", "error": e}))
+      .setMimeType(ContentService.MimeType.JSON);
+    } finally { //release lock
+        lock.releaseLock();
+    }
+  }
